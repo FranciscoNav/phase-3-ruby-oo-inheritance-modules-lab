79dda06052f767e2f364575475cc5298e73b5c8b
Francisco <fnaveira11@gmail.com>
2021-05-24 15:25:12 -0400

    commit tree 547b24d94d17037542a81b204cd2d622d65138b6

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,10 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  extend Findable
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
@@ -31,7 +32,8 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  include Paramable
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
